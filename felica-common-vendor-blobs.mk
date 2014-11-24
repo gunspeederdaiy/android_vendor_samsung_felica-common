10c40b9dc8905fb78c3fcffffc683b80c30321de
scoty755 <scoty755@gmail.com>
2014-11-24 20:19:41 +0900

    Initial commit

---
+++ felica-common-vendor-blobs.mk
@@ -0,0 +1,37 @@
+# Copyright (C) 2014 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+PRODUCT_COPY_FILES += \
+	vendor/samsung/felica-common/proprietary/app/FeliCaLock.apk:system/app/FeliCaLock.apk \
+	vendor/samsung/felica-common/proprietary/app/FeliCaRemoteLock.apk:system/app/FeliCaRemoteLock.apk \
+	vendor/samsung/felica-common/proprietary/app/FeliCaTest.apk:system/app/FeliCaTest.apk \
+	vendor/samsung/felica-common/proprietary/app/MobileFeliCaClient.apk:system/app/MobileFeliCaClient.apk \
+	vendor/samsung/felica-common/proprietary/app/MobileFeliCaMenuApp.apk:system/app/MobileFeliCaMenuApp.apk \
+	vendor/samsung/felica-common/proprietary/app/MobileFeliCaSettingApp.apk:system/app/MobileFeliCaSettingApp.apk \
+	vendor/samsung/felica-common/proprietary/app/MobileFeliCaWebPluginBoot.apk:system/app/MobileFeliCaWebPluginBoot.apk \
+	vendor/samsung/felica-common/proprietary/bin/mfdp:system/bin/mfdp \
+	vendor/samsung/felica-common/proprietary/bin/mfsc:system/bin/mfsc \
+	vendor/samsung/felica-common/proprietary/etc/felica/No_ticket.png:system/etc/felica/No_ticket.png \
+	vendor/samsung/felica-common/proprietary/etc/felica/air.png:system/etc/felica/air.png \
+	vendor/samsung/felica-common/proprietary/etc/felica/bookmark.cfg:system/etc/felica/bookmark.cfg \
+	vendor/samsung/felica-common/proprietary/etc/felica/common.cfg:system/etc/felica/common.cfg \
+	vendor/samsung/felica-common/proprietary/etc/felica/id.png:system/etc/felica/id.png \
+	vendor/samsung/felica-common/proprietary/etc/felica/mfc_falp.cfg:system/etc/felica/mfc_falp.cfg \
+	vendor/samsung/felica-common/proprietary/etc/felica/mfc_push.cfg:system/etc/felica/mfc_push.cfg \
+	vendor/samsung/felica-common/proprietary/etc/felica/mfm.cfg:system/etc/felica/mfm.cfg \
+	vendor/samsung/felica-common/proprietary/etc/felica/mfs.cfg:system/etc/felica/mfs.cfg \
+	vendor/samsung/felica-common/proprietary/etc/felica/money.png:system/etc/felica/money.png \
+	vendor/samsung/felica-common/proprietary/etc/felica/portal_icon.png:system/etc/felica/portal_icon.png \
+	vendor/samsung/felica-common/proprietary/etc/felica/toku.png:system/etc/felica/toku.png \
+	vendor/samsung/felica-common/proprietary/etc/felica/train.png:system/etc/felica/train.png
